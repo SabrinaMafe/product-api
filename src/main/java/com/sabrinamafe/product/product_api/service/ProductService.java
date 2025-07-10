3d908779578060b405b5e600eac5d374c4481f8e
Sabrina Mafé <58732017+SabrinaMafe@users.noreply.github.com>
2025-07-10 17:31:33 -0300

    Add Category and Product repositories and services with basic CRUD operations

---
+++ src/main/java/com/sabrinamafe/product/product_api/service/ProductService.java
@@ -0,0 +1,33 @@
+package com.sabrinamafe.product.product_api.service;
+
+import com.sabrinamafe.product.product_api.model.Product;
+import com.sabrinamafe.product.product_api.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Page<Product> findAll(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
